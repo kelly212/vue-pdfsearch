e70aa321f85cc4ce376cdeb377d4eae552e6b4aa
kelly <kellyreader1@gmail.com>
2026-04-22 08:07:43 -0300

    removendo db

--- netlify/functions/db.js
+++
@@ -1,7 +0,0 @@
-import pkg from 'pg'
-const { Pool } = pkg
-
-export const pool = new Pool({
-   connectionString: process.env.NETLIFY_DATABASE_URL_UNPOOLED,
-   // ssl: { rejectUnauthorized: false }
-})
